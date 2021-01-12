cbf3e03611b51175f88b908bcf1a49eb2d75b12d
Александр Заверняев <zaverclub@gmail.com>
2021-01-13 02:32:55 +0300

    Добавлен header

---
+++ components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <b class="header__title">TestList</b>
+    <Cart />
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass">
+.header
+  display: flex
+  justify-content: space-between
+
+  padding: 18px 108px 20px 88px
+
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.05)
+  border-radius: 0px 0px 8px 8px
+  &__title
+    font-size: 22px
+    color: #59606D
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,10 @@
 <template>
-  <nuxt />
+  <div>
+    <Header />
+    <nuxt />
+  </div>
 </template>
+
+<style lang="sass">
+@use "~/assets/default"
+</style>
