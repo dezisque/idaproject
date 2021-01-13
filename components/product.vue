533f5e7e6ed90d2e5a79e7da10043f0fab4eb13d
Александр Заверняев <zaverclub@gmail.com>
2021-01-13 16:08:47 +0300

    Добавлена карточка продукта

---
+++ components/product.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="product">
+    <div class="product__view">
+      <div class="product__rate">
+        <img src="~/assets/star.svg" alt="Звезда" />
+        <b>4.5</b>
+      </div>
+      <img
+        src="https://frontend-test.idaproject.com/upload/product/backpack15-067a.cc84tf.jpg"
+        class="product__image"
+      />
+      <button class="product__add-to-cart"></button>
+      <!-- <img src="~/assets/cart.svg" class="product__add-to-cart" /> -->
+    </div>
+
+    <div class="product__name">Рюкзак Louis Vuitton Discovery</div>
+    <b class="product__price">150 000р</b>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.product
+  box-sizing: border-box
+  position: relative
+
+  height: 272px
+  width: 264px
+
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.05)
+  border-radius: 8px
+
+  padding: 17px 18px 16px 16px
+
+  &__view
+    display: flex
+    align-items: center
+    justify-content: space-between
+    position: relative
+
+  &__rate
+    position: absolute
+    top: 0
+    color: #F2C94C
+    font-size: 10px
+    line-height: 13px
+
+    display: flex
+    width: 34px
+    height: 20px
+
+    b
+      margin-top: 3px
+
+  &__add-to-cart
+    position: absolute
+    top: 0
+    right: 0
+
+    outline: none
+    border: none
+
+    width: 12px
+    height: 14px
+
+    background: url('~assets/cart.svg')
+    background-size: cover
+
+    &:hover
+      background: url('~assets/cart-grey.svg')
+      cursor: pointer
+
+  &__image
+    width: 142px
+    height: 180px
+
+    display: block
+
+    margin-left: auto
+    margin-right: auto
+
+    object-fit: cover
+
+  &__name
+    color: #59606D
+    font-size: 14px
+
+    margin-top: 16px
+    margin-bottom: 6px
+
+  &__price
+    color: #1F1F1F
+    font-size: 14px
+</style>
