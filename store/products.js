5c03cfe78c1886a34407fed964443a23cd46a389
Александр Заверняев <zaverclub@gmail.com>
2021-01-15 02:48:08 +0300

    Добален store для продуктов

---
+++ store/products.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+  products: {},
+})
+
+export const mutations = {
+  setProducts(state, products) {
+    state.products = products
+  },
+  sortProductsByPrice(state) {
+    state.products = state.products.sort((a, b) => b.price - a.price)
+  },
+}
