b180dac3c3dedf1cf6347da7618c4881d6c02c7b
Александр Заверняев <zaverclub@gmail.com>
2021-01-15 03:21:42 +0300

    Добавлен счётчик продуктов в корзине

--- components/cart.vue
+++ components/cart.vue
@@ -1,14 +1,44 @@
 <template>
-  <img src="~/assets/cart.svg" alt="корзина" />
+  <div class="cart">
+    <img src="~/assets/cart.svg" alt="корзина" />
+    <b v-if="cartItemsCounter">{{ cartItemsCounter }}</b>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    cartItemsCounter() {
+      return this.$store.getters['cart/productsCount']
+    },
+  },
+}
 </script>
 
 <style lang="sass" scoped>
-img
-  cursor: pointer
-  width: 24px
-  height: 26px
+.cart
+  position: relative
+  img
+    cursor: pointer
+    width: 24px
+    height: 26px
+  b
+    width: 12px
+    height: 12px
+
+    box-sizing: border-box
+    padding-top: 1px
+
+    text-align: center
+
+    position: absolute
+    top: -3px
+    right: -3px
+
+    font-size: 8px
+    line-height: 10px
+
+    color: white
+    background-color: #959DAD
+    border-radius: 50%
 </style>
