6ee550f4740f872f89b42d9150892292bb4ec400
Александр Заверняев <zaverclub@gmail.com>
2021-01-19 21:02:11 +0300

    Правка верстки списка товара

--- pages/catalog.vue
+++ pages/catalog.vue
@@ -46,8 +46,10 @@ export default {
   padding-top: 32px
   padding-left: 88px
 
-  min-width: 248px
+  //width: 248px
+  min-width: 160px
   height: 100%
+  //box-sizing: border-box
 
   &__header
     color: #1F1F1F
